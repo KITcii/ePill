f7b649a60692d1fc925c3bfce6f5522320819d66
cs <spe.christina@gmail.com>
2019-08-30 17:49:06 +0200

    medication plan: added conditional rendering

--- src/main/frontend/src/components/medication_plan.js
+++ src/main/frontend/src/components/medication_plan.js
@@ -10,7 +10,7 @@ class MedicationPlan extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-        		
+        		drugs : []
         };
 
     }
@@ -24,20 +24,45 @@ class MedicationPlan extends React.Component {
         var tempDate = new Date();
         var date = tempDate.getDate() + '.' + (tempDate.getMonth()+1) + '.' + tempDate.getFullYear();
         var time = tempDate.getHours() + ':' + tempDate.getMinutes();
+        axios.get('/drug/list/taking')
+            .then(({data}) => {
+                this.state.drugs = data.value;
+            });
         return (
-        	<div className="container marketing no-banner">
-        		<div className='page-header'>
-        	    		<div className='btn-toolbar pull-right'>
-        	            <div className='btn-group'></div>
+        	<div>
+                {this.state.drugs.length < 1 ? (
+                    <div>
+                        <div className="container marketing no-banner">
+                            <div className='page-header'>
+                                <div className='btn-toolbar pull-right'>
+                                    <div className='btn-group'></div>
+                                </div>
+                                <h3>{t('medicationPlan')}</h3>
+                            </div>
+                            <div className="col-md-12" dangerouslySetInnerHTML={this.createMarkup(t('medicationEmpty'))} />
+                            <Link to="/drug/list"><button type="button" className="btn btn-default">{t('addDrugsToMedicationPlan')}</button></Link>
                         </div>
-                    <h3>{t('medicationPlan')}</h3>
-                    <h5>{t('for') + ' ' + date} </h5>
-                </div>
-        	    
-        		<div className="col-md-12 no-padding" dangerouslySetInnerHTML={this.createMarkup(t('medicationEmpty'))} />
+
+                    </div>
+                    ):(
+                    <div>
+                        <div className="container marketing no-banner">
+                            <div className='page-header'>
+                                <div className='btn-toolbar pull-right'>
+                                    <div className='btn-group'></div>
+                                </div>
+                                <h3 class="text-center">{t('medicationPlan')}</h3>
+                                <h5>{t('for') + ' ' + date} </h5>
+                            </div>
+                            <div className="col-md-12" dangerouslySetInnerHTML={this.createMarkup(t('medicationPlan'))} />
+                        </div>
+                    </div>
+                )
+                }
+            </div>
         		
-        		<Link to="/drug/list"><button type="button" className="btn btn-default">{t('addDrugsToMedicationPlan')}</button></Link>
-        	</div>
+
+
         );
     }
 }
